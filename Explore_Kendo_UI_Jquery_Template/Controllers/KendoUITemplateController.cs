4ed0eed57766e119f50bb3ce7cad07e81e7f23e7
Jin Vincent Necesario <lock29down@gmail.com>
2020-03-14 21:30:04 +0800

    Add project files.

---
+++ Explore_Kendo_UI_Jquery_Template/Controllers/KendoUITemplateController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Explore_Kendo_UI_Jquery_Template.Controllers
+{
+    public class KendoUITemplateController : Controller
+    {
+        // GET: KendoUITemplate
+        // This is will show the quick example of Kendo UI Template
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult InlineTemplate()
+        {
+            return View();
+        }
+
+        public ActionResult ExternalTemplate()
+        {
+            return View();
+        }
+
+        public ActionResult HtmlEncoding_DisplayValues()
+        {
+            return View();
+        }
+
+        public ActionResult Execute_Complex_JavaScriptCode()
+        {
+            return View();
+        }
+
+        public ActionResult MVVM_Kendo_UI_Template_Example()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Explore_Kendo_UI_Jquery_Template/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Explore_Kendo_UI_Jquery_Template.MvcApplication" Language="C#" %>
